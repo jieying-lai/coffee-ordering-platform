10858ad6098b62ec836d00f5523ddac79067add4
Zhi-Qing-6272 <changzhiqing1996@gmail.com>
2026-08-08 10:12:07 +0800

    Add Benefits and Activities sections to navigation

--- blog/user_posts.php
+++ blog/user_posts.php
@@ -45,6 +45,22 @@ $userPosts = $pStmt->get_result();
     <li><a href="../home/index.php">Home</a></li>
     <li><a href="../menu/index.php">Menu</a></li>
     <li><a href="index.php" class="active">Blog</a></li>
+        <li><a href="../benefits/index.php">Benefits</a></li>
+    <li>
+      <a href="../offers/index.php">Offers ▾</a>
+      <div class="dropdown">
+        <a href="../offers/index.php#drinks">Drink Offers</a>
+        <a href="../offers/index.php#food">Food Offers</a>
+        <a href="../offers/index.php#partners">Partner Promotions</a>
+      </div>
+    </li>
+    <li>
+      <a href="../activities/index.php">Activities ▾</a>
+      <div class="dropdown">
+        <a href="../activities/index.php#workshops">Coffee Workshops</a>
+        <a href="../activities/index.php#giveback">Cozy Give-Back</a>
+      </div>
+    </li>
     <li><a href="../contact/index.php">Contact</a></li>
     <li><a href="../cart/index.php">Cart</a></li>
     <?php if ($isLoggedIn): ?>
