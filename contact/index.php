1f3e4c8668c89e6624180d32becf6f75406da985
Zhi-Qing-6272 <changzhiqing1996@gmail.com>
2026-08-08 10:15:10 +0800

    Add navigation links for benefits and offers

--- contact/index.php
+++ contact/index.php
@@ -32,6 +32,22 @@ $info  = $conn->query('SELECT * FROM contact_info WHERE id = 1')->fetch_assoc();
       </div>
     </li>
     <li><a href="../blog/index.php">Blog</a></li>
+        <li><a href="../benefits/index.php">Benefits</a></li>
+    <li>
+      <a href="../offers/index.php">Offers ▾</a>
+      <div class="dropdown">
+        <a href="../offers/index.php#drinks">Drink Offers</a>
+        <a href="../offers/index.php#food">Food Offers</a>
+        <a href="../offers/index.php#partners">Partner Promotions</a>
+      </div>
+    </li>
+    <li>
+      <a href="../activities/index.php">Activities ▾</a>
+      <div class="dropdown">
+        <a href="../activities/index.php#workshops">Coffee Workshops</a>
+        <a href="../activities/index.php#giveback">Cozy Give-Back</a>
+      </div>
+    </li>
     <li><a href="index.php" class="active">Contact</a></li>
     <li><a href="../cart/index.php">Cart</a></li>
         <!-- DYNAMIC NAVIGATION LINK -->
@@ -41,6 +57,7 @@ $info  = $conn->query('SELECT * FROM contact_info WHERE id = 1')->fetch_assoc();
         <a href="../profile/index.php"><?php echo htmlspecialchars($_SESSION['fullname']); ?> ▾</a>
         <div class="dropdown">
           <a href="../profile/index.php">My Profile</a>
+          <a href="../rewards/index.php">Cozy Rewards</a>
           <a href="../logout.php">Logout</a>
         </div>
       </li>
